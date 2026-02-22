7f84006acda205d28679b840515fe0e4d64580d5
JEYALAKSHMI M <m.jeylaksh7337@gmail.com>
2026-02-22 17:54:23 +0530

    Fix database pool import in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const pool = require('./config/db');
 const authRoutes = require('./routes/auth');
 const skillRoutes = require('./routes/skills');
 const studentRoutes = require('./routes/students');
